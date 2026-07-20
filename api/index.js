eb00bccbfd5caa90dcdc4e7f8ea93451552431f5
tungleecode <vuapekka@gmail.com>
2026-07-20 16:12:49 +0700

    Fix syntax error causing 500 in Vercel

--- api/index.js
+++ api/index.js
@@ -238,6 +238,6 @@ module.exports = app;
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-        console.log(\`Server is running locally at http://localhost:\${PORT}\`);
+        console.log(`Server is running locally at http://localhost:${PORT}`);
     });
 }
